43e84b1095b5bcd664748f62c9d54469c26ae0bf
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-07-18 15:36:16 +0300

    Add switch statement for male gender

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let CC, YY, MM, DD, d, dayOfBirth, akanName;
-let dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday"];
+let weekDays = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday"];
 let maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 let femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 let myForm = document.forms.myForm;
@@ -27,5 +27,28 @@ function choosedGender(){
     return false;
   }
 
-  
+  switch(gender){
+    case "male":
+      if(dayOfBirth == 1){
+        alert("Your were born on " + weekDays[0] + " and your akan name is " + maleNames[0] + ".");
+      }
+      else if(dayOfBirth == 2){
+        alert("Your were born on " + weekDays[1] + " and your akan name is " + maleNames[1] + ".");
+      }
+      else if(dayOfBirth == 3){
+        alert("Your were born on " + weekDays[2] + " and your akan name is " + maleNames[2] + ".");
+      }
+      else if(dayOfBirth == 4){
+        alert("Your were born on " + weekDays[3] + " and your akan name is " + maleNames[3] + ".");
+      }
+      else if(dayOfBirth == 5){
+        alert("Your were born on " + weekDays[4] + " and your akan name is " + maleNames[4] + ".");
+      }
+      else if(dayOfBirth == 6){
+        alert("Your were born on " + weekDays[5] + " and your akan name is " + maleNames[5] + ".");
+      }
+      else if(dayOfBirth == -0){
+        alert("Your were born on " + weekDays[6] + " and your akan name is " + maleNames[0] + ".");
+      }
+  }
 }
